a37a51d52bbf00058ccf21796484d6b34922ef49
unknown <zaidorade5@gmail.com>
2020-10-28 16:56:00 -0400

    Created more styling for jumbotron with styled components, and created globalstyles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import react from "react";
 import React from 'react';
-import Jumbotron from './components/jumbotron/index';
+import jumboData from './fixtures/jumbo.json';
+import Jumbotron from './components/jumbotron';
 
 export default function App() {
   return (
-    <Jumbotron/>
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt}/>
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
+import {GlobalStyles} from './global-styles';
 
-render(<App />,document.getElementById('root'));
+render(<>
+<GlobalStyles />
+<App />
+</>,document.getElementById('root'));
