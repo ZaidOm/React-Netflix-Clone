352a455cdb941a8ff8300933d0eaa8173f65cbeb
unknown <zaidorade5@gmail.com>
2020-10-28 16:25:21 -0400

    Created small file structure for jumbotron and create styled components style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import react from "react";
 import React from 'react';
+import Jumbotron from './components/jumbotron/index';
 
 export default function App() {
   return (
-    <p>Hello</p>
+    <Jumbotron/>
   );
 }
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Inner } from './styles/jumbotron';
+
+export default function Jumbotron() {
+    return (
+        <Inner>
+            <p>Hello again!</p>
+        </Inner>
+    )
+}
