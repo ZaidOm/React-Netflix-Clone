27a6df984d721cc88efffa4b9bc09ab759fc3d31
unknown <zaidorade5@gmail.com>
2020-10-29 02:05:38 -0400

    Browse component update

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,88 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { SelectProfileContainer } from './profiles';
+import { FireBaseContext } from '../context/firebase';
+import { Header, Loading } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
+import { FooterContainer } from './footer';
+import Fuse from 'fuse.js';
 
 export function BrowseContainer({ slides }) {
-    return <p>Hello</p>
+    const [category, setCategory] = useState('series');
+    const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
+
+    const { firebase } = useContext(FireBaseContext);
+    const user = firebase.auth().currentUser || {};
+
+    useEffect(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 3000);
+      }, [profile.displayName]);
+    
+      useEffect(() => {
+        setSlideRows(slides[category]);
+      }, [slides, category]);
+    
+      useEffect(() => {
+        const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
+        const results = fuse.search(searchTerm).map(({ item }) => item);
+    
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+          setSlideRows(results);
+        } else {
+          setSlideRows(slides[category]);
+        }
+      }, [searchTerm]);
+
+      return profile.displayName ? (
+        <>
+          {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+    
+          <Header src="joker1" dontShowOnSmallViewPort>
+            <Header.Frame>
+              <Header.Group>
+                <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+                <Header.TextLink active={category === 'series' ? 'true' : 'false'} onClick={() => setCategory('series')}>
+                  Series
+                </Header.TextLink>
+                <Header.TextLink active={category === 'films' ? 'true' : 'false'} onClick={() => setCategory('films')}>
+                  Films
+                </Header.TextLink>
+              </Header.Group>
+              <Header.Group>
+                <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                <Header.Profile>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.Dropdown>
+                    <Header.Group>
+                      <Header.Picture src={user.photoURL} />
+                      <Header.TextLink>{user.displayName}</Header.TextLink>
+                    </Header.Group>
+                    <Header.Group>
+                      <Header.TextLink onClick={() => firebase.auth().signOut()}>Sign out</Header.TextLink>
+                    </Header.Group>
+                  </Header.Dropdown>
+                </Header.Profile>
+              </Header.Group>
+            </Header.Frame>
+    
+            <Header.Feature>
+              <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+              <Header.Text>
+                Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+                City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+                futile attempt to feel like he's part of the world around him.
+              </Header.Text>
+              <Header.PlayButton>Play</Header.PlayButton>
+            </Header.Feature>
+          </Header>
+          <FooterContainer />
+        </>
+      ) : (
+        <SelectProfileContainer user={user} setProfile={setProfile} />
+      );
 }
