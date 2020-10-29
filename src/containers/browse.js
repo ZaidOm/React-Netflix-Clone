860d66db8353c43d4b9344d12f267e0c7bbd72a9
unknown <zaidorade5@gmail.com>
2020-10-29 17:37:15 -0400

    Created player and card components

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FireBaseContext } from '../context/firebase';
-import { Header, Loading } from '../components';
+import { Header, Loading, Card, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 import { FooterContainer } from './footer';
@@ -80,6 +80,30 @@ export function BrowseContainer({ slides }) {
               <Header.PlayButton>Play</Header.PlayButton>
             </Header.Feature>
           </Header>
+          <Card.Group>
+            {slideRows.map((slideItem) => (
+            <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                <Card.Title>{slideItem.title}</Card.Title>
+                <Card.Entities>
+                {slideItem.data.map((item) => (
+                    <Card.Item key={item.docId} item={item}>
+                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                    <Card.Meta>
+                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                        <Card.Text>{item.description}</Card.Text>
+                    </Card.Meta>
+                    </Card.Item>
+                ))}
+                </Card.Entities>
+                <Card.Feature category={category}>
+                <Player>
+                    <Player.Button />
+                    <Player.Video src="/videos/bunny.mp4" />
+                </Player>
+                </Card.Feature>
+            </Card>
+            ))}
+            </Card.Group>
           <FooterContainer />
         </>
       ) : (
