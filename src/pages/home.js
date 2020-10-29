17d532235a53ce414ef1a606c8b1e2eb43a68976
unknown <zaidorade5@gmail.com>
2020-10-29 00:26:09 -0400

    Finished prototype netflix homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { OptForm } from '../components';
+import { Feature, OptForm } from '../components';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 import { JumbotronContainer } from '../containers/jumbotron';
@@ -9,12 +9,16 @@ export default function Home() {
     return(
         <>
             <HeaderContainer>
-                <OptForm>
-                    <OptForm.Input placeholder="Email address" />
-                    <OptForm.Button>Try it now</OptForm.Button>
-                    <OptForm.Break />
-                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
-                </OptForm>
+                <Feature>
+                    <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                    <OptForm>
+                        <OptForm.Input placeholder="Email address" />
+                        <OptForm.Button>Get Started</OptForm.Button>
+                        <OptForm.Break />
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                    </OptForm>
+                </Feature>
             </HeaderContainer>
             <JumbotronContainer/>
             <FaqsContainer />
