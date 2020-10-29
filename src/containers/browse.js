e80091cd75183ea108245a64e7bcaed112ee372c
unknown <zaidorade5@gmail.com>
2020-10-29 17:50:09 -0400

    Finished and tweaked some minor bugs

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -98,7 +98,7 @@ export function BrowseContainer({ slides }) {
                 <Card.Feature category={category}>
                 <Player>
                     <Player.Button />
-                    <Player.Video src="/videos/bunny.mp4" />
+                    <Player.Video src="/videos/beans.mp4" />
                 </Player>
                 </Card.Feature>
             </Card>
